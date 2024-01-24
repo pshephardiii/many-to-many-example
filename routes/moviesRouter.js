86a192808d2832d694911bd1f3781c5d4489caa3
Paul Shephard <paulshephard598@gmail.com>
2024-01-23 19:41:43 -0500

    added movie router code

--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,4 +2,9 @@ const express = require('express')
 const router = express.Router()
 const movieController = require('../controllers/moviesController')
 
+router.post('/', movieController.createMovie)
+router.get('/', movieController.indexMovies)
+router.get('/:id', movieController.showMovie)
+router.post('/:movieId/performers/:performerId', movieController.addPerformer)
+
 module.exports = router
